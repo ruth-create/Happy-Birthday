492a3300576342f7e47024f1079fe8cd94424210
ruth-create <80636279+ruth-create@users.noreply.github.com>
2021-08-18 09:46:17 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+var canvas = new fabric.Canvas('canvas1');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+fabric.Image.fromURL('BirthdayImage.jpg',function(Img){
+block_image_object=Img;
+block_image_object.scaleToWidth(800);
+block_image_object.scaleToHeight(500);
+block_image_object.set({
+top:0,
+left:0
+});
+canvas.add(block_image_object);
+});	
+	
+}
+
+function playSound(){
+	x.play();
+}
